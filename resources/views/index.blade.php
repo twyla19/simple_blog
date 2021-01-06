ef52be01d87dd216226e39c38894c843990e74d0
twyla19 <55038483+twyla19@users.noreply.github.com>
2021-01-06 09:12:35 +0700

    homepage front end

---
+++ resources/views/index.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - GO BLOG</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
+        <div class="container"><a class="navbar-brand" href="/">GO BLOG</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div
+                class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="/">BERANDA</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="/">TENTANG KAMI</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="/">KONTAK</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="/">LOGIN</a></li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+    <header class="masthead" style="background-image:url('/img/home-bg.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="site-heading">
+                        <h1>GO BLOG</h1><span class="subheading">BAPAK LO LONT</span></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-lg-8">
+                <div class="post-preview">
+                    <a href="#">
+                        <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
+                        <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                    </a>
+                    <p class="post-meta">Posted by&nbsp;<a href="#">Penulis Handal on September 24, 2018</a></p>
+                </div>
+                <hr>
+                <div class="post-preview">
+                    <a href="#">
+                        <h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2>
+                    </a>
+                    <p class="post-meta">Posted by&nbsp;<a href="#">Penulis Handal on September 18, 2018</a></p>
+                </div>
+                <hr>
+                <div class="post-preview">
+                    <a href="#">
+                        <h2 class="post-title"> Science has not yet mastered prophecy</h2>
+                        <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
+                    </a>
+                    <p class="post-meta">Posted by&nbsp;<a href="#">Penulis Handal on August 24, 2018</a></p>
+                </div>
+                <hr>
+                <div class="post-preview">
+                    <a href="#">
+                        <h2 class="post-title">Failure is not an option</h2>
+                        <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
+                    </a>
+                    <p class="post-meta">Posted by&nbsp;<a href="#">Penulis Handal on July 8, 2018</a></p>
+                </div>
+                <hr>
+                <div class="clearfix"><button class="btn btn-primary float-right" type="button">Older Posts&nbsp;⇒</button></div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <p class="text-muted copyright">Copyright&nbsp;©&nbsp;GO BLOG 2021</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/clean-blog.js"></script>
+</body>
+
+</html>
